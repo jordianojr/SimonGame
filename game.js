cae54b30fdcfd18e5a6aca146342b7dc7e62913e
jordianojr <164317842+jordianojr@users.noreply.github.com>
2024-06-11 18:57:15 +0800

    Update game.js

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ function startOver(){
 function makeSound(key){
     switch (key) {
         case key:
-            var key = new Audio("https://github.com/jordianojr/SimonGame/blob/main/" + key + ".mp3");
+            var key = new Audio("https://github.com/jordianojr/SimonGame/blob/main/" + key + ".mp3?raw=true");
             key.play();
         break;
 
